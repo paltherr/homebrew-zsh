fd9a619048e488c1cadbd055ca7424e32fb75820
Philippe Altherr <philippe.altherr@gmail.com>
2022-03-30 00:37:11 +0200

    Add zabort formula.

---
+++ Formula/zabort.rb
@@ -0,0 +1,13 @@
+class Zabort < Formula
+  desc "Abort functionality for zsh"
+  homepage "https://github.com/paltherr/zabort"
+  url "https://github.com/paltherr/zabort/archive/v0.1.0.tar.gz"
+  sha256 "16224e9d0df6386f92a9e7fa5c099d81c02e5c8109687d026fcc74fe66a65c07"
+  head "https://github.com/paltherr/zabort.git", :branch => "main"
+
+  def install
+    bin.install "src/bin/zabort.zsh"
+    zsh_function.install "src/functions/abort"
+    zsh_function.install "src/functions/usage"
+  end
+end
