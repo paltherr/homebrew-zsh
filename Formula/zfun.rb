835a8ff2602e32f66fbc40036709701fd7c008b5
Philippe Altherr <philippe.altherr@gmail.com>
2022-09-14 21:33:57 +0200

    zfun 0.1.1

--- Formula/zfun.rb
+++ Formula/zfun.rb
@@ -1,8 +1,8 @@
 class Zfun < Formula
   desc "Enhanced function support for zsh"
   homepage "https://github.com/paltherr/zfun"
-  url "https://github.com/paltherr/zfun/archive/v0.1.0.tar.gz"
-  sha256 "1a4e9d14620bf6e53aaf86047e60e31e02a1f600bff1d8dc422a16fa6594f6ce"
+  url "https://github.com/paltherr/zfun/archive/v0.1.1.tar.gz"
+  sha256 "aded0115638f5d223983f0a316316f433728c2e14b4abd3bd87213868521ad57"
   head "https://github.com/paltherr/zfun.git", :branch => "main"
 
   def install
