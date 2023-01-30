51c952890d650902bf3a6b27cede37c89d293dcf
Philippe Altherr <philippe.altherr@gmail.com>
2023-01-30 23:42:35 +0100

    zbench 0.1.0

--- Formula/zbench.rb
+++ Formula/zbench.rb
@@ -1,8 +1,8 @@
 class Zbench < Formula
   desc "Zsh code benchmarking"
   homepage "https://github.com/paltherr/zbench"
-  url "https://github.com/paltherr/zbench/archive/v0.0.0.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  url "https://github.com/paltherr/zbench/archive/v0.1.0.tar.gz"
+  sha256 "67786c9e235adb480f7a93f9473f10ada04b7c9c4ae77c96c4a31efe96f28a9b"
   head "https://github.com/paltherr/zbench.git", :branch => "main"
   license "MIT"
 
