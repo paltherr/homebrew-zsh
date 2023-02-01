f76b4aa1cc630a0b8b303953579e0e5920e21fe8
Philippe Altherr <philippe.altherr@gmail.com>
2023-02-01 01:29:56 +0100

    zmisc 0.1.0

--- Formula/zmisc.rb
+++ Formula/zmisc.rb
@@ -1,8 +1,8 @@
 class Zmisc < Formula
   desc "Miscellaneous Zsh Utilities"
   homepage "https://github.com/paltherr/zmisc"
-  url "https://github.com/paltherr/zmisc/archive/v0.0.0.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  url "https://github.com/paltherr/zmisc/archive/v0.1.0.tar.gz"
+  sha256 "111ec6ae9eb11601549a870da67ba3e2b92ef60477cf7a3a18bccb9ae6529ea9"
   head "https://github.com/paltherr/zmisc.git", :branch => "main"
   license "MIT"
 
